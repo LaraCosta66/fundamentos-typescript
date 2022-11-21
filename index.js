1d793aaaa546f3acce77eefadf5b73ccf288a4dc
LaraCosta66 <larasmila64@gmail.com>
2022-11-21 12:53:44 -0300

    feat: variaveis e tipos

--- index.js
+++
@@ -1,6 +0,0 @@
-const soma = (a, b) =>{
-    console.log( a + b)
-}
-
-soma(3,4)
-soma('lara', 'Samila')
---
+++ variaveis.ts
@@ -0,0 +1,10 @@
+// declaracao de variaveis
+
+var a = "a"; // escopo global
+let b: string = "b"; // pode ser alterado ao longo do script
+const c = "c"; //imutavel
+
+//Tipos primitivos
+let A: string = "a";
+let B: number = 3;
+let x: boolean = false;
