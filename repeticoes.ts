a3185242aac6d11cee00eba85f230c57fd872835
LaraCosta66 <larasmila64@gmail.com>
2022-11-22 11:40:45 -0300

    feat: estrutura de repeticao

---
+++ repeticoes.ts
@@ -0,0 +1,13 @@
+// for recebe 3 parametros
+// estado inicial, onde a estrutura vai parar, e qual incremento vai ser feito na estrutura
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+//while recebe como parametro uma condicao
+
+let n = 2;
+while (n < 5) {
+  console.log(n);
+  n++;
+}
