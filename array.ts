b11928f42fcb642e4c311065c308be493695a9a1
LaraCosta66 <larasmila64@gmail.com>
2022-11-22 14:03:03 -0300

    feat: funcoes de add e remover elementos no array

--- array.ts
+++ array.ts
@@ -1,10 +1,15 @@
 //A variavel array recebe um tipo de array numerico
-const array: Array<number> = [1, 2, 3, 4, 5];
+const array: Array<number> = [1, 2, 3, 4];
 // indica que é um array de string
 const stringArray: string[] = ["a", "b", "c"];
 
 console.log(array.length);
 
+//Adicionar elemento na ultiam posiçao
+array.push(5);
+
+//remove o ultimo elemento
+array.pop();
 const buscaNum = array.find((num) => num === 4);
 
 console.log(buscaNum);
